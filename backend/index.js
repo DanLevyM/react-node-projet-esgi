7fb57bc373959d0e30e0af02f1779f1ebec29707
OdessaCh <odessa.chesneau@gmail.com>
2022-07-26 12:10:51 +0200

    WIP Add websocket

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,21 @@ const AdminRouter = require('./server/routes/admin-router');
 const ReportRouter = require('./server/routes/report-router');
 const RelationsRouter = require('./server/routes/relations-router');
 const AnalyticsRouter = require('./server/routes/analytics-router');
+const { Server } = require('socket.io');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3001;
 
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
+
+const server = require('http').Server(app);
+const io = new Server(server);
+
 
 app.use('/api/v1/friends', RelationsRouter);
 app.use('/api/v1/users', UserRouter);
@@ -30,9 +35,18 @@ app.use('/api/v1/analytics', AnalyticsRouter);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	socket.on('chat message', (msg) => {
+		console.log('message: ' + msg);
+	});
+});
+
+server.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
+
+
 
 // Uncomment to check routes
 const expressListRoutes = require('express-list-routes');
